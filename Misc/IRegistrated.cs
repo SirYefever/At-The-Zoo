520fae7413671b52a8c9dcbacc99faf9158364e7
Cherry on a cake <ninjaxxxflame@gmail.com>
2024-05-31 13:20:01 +0700

    Required files added to GIT.

---
+++ Misc/IRegistrated.cs
@@ -0,0 +1,8 @@
+﻿
+namespace At_The_Zoo_Wpf.Misc
+{
+    public interface IRegistrated
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ Models/RegistryControlModel.cs
@@ -0,0 +1,30 @@
+﻿using At_The_Zoo_Wpf.Misc;
+using System.ComponentModel;
+
+namespace At_The_Zoo_Wpf.Models
+{
+    public class RegistryControlModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged = (sender, e) => 
+        {};
+
+        public MainModel MainControl { get; set; }
+
+        private Dictionary<Guid, object> _registry = new();
+
+        public Dictionary<Guid, object> Registry { get => _registry; }
+
+        public void AddObject(IRegistrated obj)
+        {
+            Guid newGuid = Guid.NewGuid();
+            obj.Id = newGuid;
+            _registry.Add(newGuid, (object)obj);
+        }
+
+        public void RemoveObject(Guid id)
+        {
+            _registry.Remove(id);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Registry)));
+        }
+    }
+}
